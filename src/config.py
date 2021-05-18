75db79b67f747f236e16aa6484c2b307ff63541a
Runge, Jan <J.Runge@wertgarantie.com>
2021-05-18 13:15:51 +0200

    increase steering power

--- src/config.py
+++ src/config.py
@@ -7,7 +7,7 @@ pin_us_trigger=18
 pin_us_echo=24
 pin_us_led = 17
 
-max_power_steering = 0.3
+max_power_steering = 0.35
 increment_time_drive=0.068
 increment_time_steering=0.1
 min_power_drive = 0.2
